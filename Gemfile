83c6088d31e4e648c7325507d8b098458a57b305
Yuhan Dai <yud039@ucsd.edu>
2017-04-14 17:40:22 -0700

    added a profile page

--- Gemfile
+++ Gemfile
@@ -45,3 +45,7 @@ group :development do
   gem 'spring'
 end
 
+
+# extra gems
+gem 'bootstrap-sass', '~> 3.2.0'
+gem 'autoprefixer-rails'
---
+++ test/controllers/my_profile_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class MyProfileControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
